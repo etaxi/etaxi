5afb13356b754a46f031e83b84bd2a13aae3e126
AleksBod <aleksejs.bodaks@gmail.com>
2016-03-03 23:40:24 +0200

    JUnit test for Password hash metods

---
+++ src/test/java/business/PasswordTest.java
@@ -0,0 +1,24 @@
+package business;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Aleks on 03.03.2016.
+ */
+public class PasswordTest {
+
+    @Test
+         public void testGetSaltedHash() throws Exception {
+        String pass = Password.getSaltedHash("abcda");
+        assertTrue(Password.check("abcda", pass));
+    }
+
+    @Test
+    public void testGetSaltedHash2() throws Exception {
+        String pass = Password.getSaltedHash("abcda");
+        assertTrue(!Password.check("aaaaa2344", pass));
+    }
+
+}

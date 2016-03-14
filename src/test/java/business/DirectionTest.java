5c31e0e9ca9a03fb9933847cfe6debb1f29f8d2e
GenadyZalesky <genady.zalesky@gmail.com>
2016-03-14 11:37:52 +0200

    Test of the Direction class.

---
+++ src/test/java/business/DirectionTest.java
@@ -0,0 +1,47 @@
+package business;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Genady Zalesky on 14.03.2016
+ */
+public class DirectionTest {
+
+    @Test
+    public void pointsAdded(){
+
+        Direction direction = new Direction("Latvija, Rīga, Āzenes iela, 12", "Latvija, Rīga, Kronvalda bulvāris, 1");
+        final Map<String, String> params = direction.putParameters();
+        assertEquals("Latvija, Rīga, Āzenes iela, 12", params.get("origin"));
+        assertEquals("Latvija, Rīga, Kronvalda bulvāris, 1", params.get("destination"));
+    }
+
+    @Test
+    public void urlReceived(){
+
+        Direction direction = new Direction("Latvija, Rīga, Āzenes iela, 12", "Latvija, Rīga, Kronvalda bulvāris, 1");
+        final Map<String, String> params = direction.putParameters();
+        String url = direction.generateURL(params);
+        String urlFirstCharacters = url.substring(0, Math.min(url.length(), 90));
+        assertEquals("http://anonymouse.org/cgi-bin/anon-www.cgi/http://maps.googleapis.com/maps/api/directions/", urlFirstCharacters);
+    }
+
+    @Test
+    public void isDistance() throws IOException, JSONException {
+
+        Direction direction = new Direction("Latvija, Rīga, Āzenes iela, 12", "Latvija, Rīga, Kronvalda bulvāris, 2");
+        final Map<String, String> params = direction.putParameters();
+        String url = direction.generateURL(params);
+        JSONObject response = direction.getResponse(url);
+        JSONObject location = direction.getLocation(response);
+        String distance = direction.distance(location);
+        assertEquals("2,6 км", distance);
+    }
+}

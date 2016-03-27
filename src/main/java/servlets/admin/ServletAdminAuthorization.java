d21ff564075fb9e60abac5b5c8a40df5b1dce974
GenadyZalesky <genady.zalesky@gmail.com>
2016-03-27 14:19:09 +0300

    Servlet for admin authorization created.

---
+++ src/main/java/servlets/admin/ServletAdminAuthorization.java
@@ -0,0 +1,57 @@
+package servlets.admin;
+
+import dao.AdminDAO;
+import dao.jdbc.AdminDAOImpl;
+import dao.jdbc.DBConnection;
+import entity.Admin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by Genady Zalesky on 27.03.2016
+ */
+
+@WebServlet(name = "ServletAdminAuthorization" , urlPatterns = {"/admin/authorization"})
+public class ServletAdminAuthorization extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        DBConnection dbService = new DBConnection();
+        AdminDAO adminDAO = new AdminDAOImpl(dbService.getConnection(), dbService.getDatabaseName());
+        Admin adminDataSet = null;
+        try {
+            adminDataSet = adminDAO.getByLogin(login);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (adminDataSet == null || !adminDataSet.getPassword().equals(password)) {
+            request.setAttribute("message", "Wrong username or password!");
+            request.getRequestDispatcher("/admin/authorization.jsp").forward(request, response);
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        // сохраняем логин админа в сессию, для дальнейшем идентификации клиента в системе
+        request.getSession().setAttribute("customerId", adminDataSet.getAdminId());
+
+        request.setAttribute("message", "Authorization successful: " + adminDataSet.getName());
+        request.getRequestDispatcher("/admin/menuAdmin.jsp").forward(request, response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

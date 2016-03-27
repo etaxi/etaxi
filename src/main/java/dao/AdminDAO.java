fd61dfc883dbf5724b6b4d9ba39c88d8a5a3c134
GenadyZalesky <genady.zalesky@gmail.com>
2016-03-27 13:57:03 +0300

    AdminDAO class created.

--- src/main/java/dao/AdminDAO.java
+++ src/main/java/dao/AdminDAO.java
@@ -1,7 +1,32 @@
 package dao;
 
-/**
- * Created by Genady Zalesky on 27.03.2016
+import entity.Admin;
+import entity.Taxi;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/** Проект etaxi
+ * Интерфейс для реализации управления объектами класса Admin
  */
+
 public interface AdminDAO {
+
+    /** Возвращает объект соответствующий записи с первичным ключом key или null */
+    Taxi getById(long id) throws SQLException;
+
+    /** Возвращает объект соответствующий записи по логину */
+    Taxi getByLogin(String login) throws SQLException;
+
+    /** Сохраняет состояние объекта Admin в базе данных (если ID нет, создаем новую запись) */
+    long update(Admin admin) throws SQLException;
+
+    /** Удаляет запись об объекте Admin из базы данных */
+    void delete(Admin admin) throws SQLException;
+
+    /** Возвращает список объектов Admin соответствующих всем записям в базе данных */
+    List<Admin> getAll() throws SQLException;
+
+    /** Создает таблицу в базе данных для хранения объектов класса Admin */
+    void createTable() throws SQLException;
 }

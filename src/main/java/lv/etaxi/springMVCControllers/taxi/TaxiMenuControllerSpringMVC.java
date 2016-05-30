f78bfd93e02803b1d5484e46d493f290f5289ff4
lazorkins <lazorkin@mits.lv>
2016-05-30 21:39:05 +0300

    //Внедряем Spring Security в наш проект (userDetailsService)

--- src/main/java/lv/etaxi/springMVCControllers/taxi/TaxiMenuControllerSpringMVC.java
+++ src/main/java/lv/etaxi/springMVCControllers/taxi/TaxiMenuControllerSpringMVC.java
@@ -1,7 +1,12 @@
 package lv.etaxi.springMVCControllers.taxi;
 
-import lv.etaxi.MVC.MVCController;
-import lv.etaxi.MVC.MVCModel;
+import lv.etaxi.business.TaxiManager;
+import lv.etaxi.dto.TaxiDTO;
+import lv.etaxi.dto.СonvertorDTO;
+import lv.etaxi.entity.Taxi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 
 /**
  * Created by Aleks on 01.04.2016.
@@ -16,20 +22,34 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class TaxiMenuControllerSpringMVC {
 
+    @Autowired
+    TaxiManager taxiManagerImpl;
+
+    @Autowired
+    СonvertorDTO convertorDTO;
 
     @RequestMapping(value = "/taxi", method = {RequestMethod.GET})
-    public ModelAndView processGetRequest(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView processGetRequest(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+
+        Object userDetailsImpl = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String userName = ((User) userDetailsImpl).getUsername();
+
+        Taxi currentTaxi = taxiManagerImpl.findByLogin(userName);
+
+        if (currentTaxi != null) {
+            TaxiDTO currentTaxiDTO = convertorDTO.convertTaxiToDTO(currentTaxi);
+            request.getSession().setAttribute("taxiDTO", currentTaxiDTO);
+        }
 
         ModelAndView modelAndView = new ModelAndView("/taxi/TaxiMenu", "model", null);
         return modelAndView;
-
     }
 
     @RequestMapping(value = "/taxi", method = {RequestMethod.POST})
     public ModelAndView processPostRequest(HttpServletRequest request, HttpServletResponse response) {
+
         ModelAndView modelAndView = new ModelAndView("/taxi/TaxiMenu", "model", null);
         return modelAndView;
-
     }
 
 }

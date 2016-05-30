507bcbe74772ab67542d6b848fef0e3cdf473d3f
GenadyZalesky <genady.zalesky@gmail.com>
2016-05-30 14:16:47 +0300

    AdminDTO class created.

---
+++ src/main/java/lv/etaxi/dto/AdminDTO.java
@@ -0,0 +1,55 @@
+package lv.etaxi.dto;
+
+/**
+ * Created by Genady Zalesky on 30.05.2016
+ */
+public class AdminDTO {
+
+    private Long adminId;   // Идентификатор
+    private String name;   // Имя, Фамилия
+    private String login;       // логин
+    private String password;    // пароль
+
+    public AdminDTO(Long adminId, String name, String login, String password) {
+
+        this.adminId = adminId;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+
+        return "{" +
+                "\"adminId\":" + "\"" + adminId + "\"" +
+                ", \"name\":" + "\"" + name + "\"" +
+                ", \"login\":" + "\"" + login + "\"" +
+                ", \"password\":" + "\"" + password + "\"" +
+                '}';
+    }
+}
